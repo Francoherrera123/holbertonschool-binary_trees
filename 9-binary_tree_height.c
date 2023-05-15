9a5fc01e00ed3826d53ad61839a955e21141c3e6
root <Francoherrera123>
2023-05-15 09:33:29 -0700

    Task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height_r -  measures the height of a binary tree in recursive
+ * @tree: is a pointer to the root node of the tree to measure the height
+ * @is_first: 0 - Isn't first node | 1 - Is first node
+ * Return: return height of a binary tree
+ */
+size_t binary_tree_height_r(const binary_tree_t *tree, int is_first)
+{
+	int i = 0, j = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		i = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		j = 1 + binary_tree_height(tree->right);
+	return ((i < j) ? j : i);
+}
