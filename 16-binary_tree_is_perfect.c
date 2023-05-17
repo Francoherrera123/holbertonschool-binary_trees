94fdc02f7b53863f577937d86e79e9c313ab0283
root <Francoherrera123>
2023-05-16 19:15:02 -0700

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,6 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
+#include "12-binary_tree_leaves.c"
 /**
  * binary_tree_is_perfect - fuction that check if a tree is perfect
  * @tree: data
